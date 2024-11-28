59fc376dbe673069ea5e7b6d04313fbbf54486a8
Sumit Jadhav <sumitjadav2525@gmail.com>
2024-11-28 13:20:10 +0530

    pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer/Footer'
+import Cards from './components/Cards/Cards'
+import { BrowserRouter } from "react-router";
 
 function App() {
   return (
     <>App
     <Navbar />
+    <Cards />
+    <Footer />
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router";
+import { Routes, Route } from "react-router";
+import Cards from './components/Cards/Cards.jsx';
+import Navbar from './components/Navbar.jsx';
+import Aboutus from './components/Aboutus/Aboutus.jsx';
+
+import Footer from './components/Footer/Footer.jsx';
+import ContactUs from './components/ContactUs/ContactUs.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/navbar" element={<Navbar />} />
+      <Route path="/footer" element={<Footer />} />
+      <Route path="/aboutus" element={<Aboutus />} />
+      <Route path="/contctus" element={<ContactUs />} />
+
+
+      <Route path="/card" element={<Cards />} />
+
+    </Routes>
+  </BrowserRouter>,
 )
